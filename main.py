c484eae07b3be3e047f3a0627ad959417afb5803
daydien12 <nguyenquangbien69@gmail.com>
2021-08-15 22:32:05 +0700

    đã thêm được giao diện đăng kí
    thêm được giao diện đăng kí

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from PyQt5.QtWidgets import QMainWindow
 from MQTT_TEST import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 import glob
+import json
+
 
 class MainWindow:
     def __init__(self, parent=None):
@@ -34,7 +36,10 @@ class MainWindow:
         self.uic.Learn_Button_Back.clicked.connect(self.Learn_Button_Back)
         self.uic.Learn_Button_Exit.clicked.connect(self.Show_Screen_Home)
         self.uic.Learn_Button_Next.clicked.connect(self.Learn_Button_Next)
-        self.convert(self.Name_File_PDF)
+        self.Learn_convert_PDF_TO_IMAGE(self.Name_File_PDF)
+        # Khối button của màn hình Test
+        self.uic.screen_Test_Display_InputInfor_Button_Exit.clicked.connect(self.Show_Screen_Home)
+        self.uic.screen_Test_Display_InputInfor_Button_Start.clicked.connect(self.screen_Test_Show_Display_Exam)
 
     def show(self):
         self.main_win.show()
@@ -57,18 +62,8 @@ class MainWindow:
     def Show_Screen_Information(self):
         self.uic.stackedWidget.setCurrentWidget(self.uic.screen_Information)
 
-    def convert(self,name):
-        outputDir = self.Path_Image_Screen_Learn + '/'
-        if not os.path.exists(outputDir):
-            os.makedirs(outputDir)
-        pages = convert_from_path(name, 500, size=(self.Learn_Size_X_Image_PDF,self.Learn_Size_Y_Image_PDF))
-        counter = 1
-        for page in pages:
-            myfile = outputDir + self.Name_image_Screen_Learn + str(counter) + '.jpg'
-            counter = counter + 1
-            page.save(myfile, "JPEG")
 
-#-----------------------------------------(điều khiển next hoặc back của giao diện learn)-----------------------------------------------------------------------------#
+#-----------------------------------------(Giao Diện học lý thuyết)-----------------------------------------------------------------------------#
     def Learn_Button_Next(self):
         if( self.Count_next_image < self.Number_Image_Learn):
             self.Count_next_image = self.Count_next_image + 1
@@ -90,11 +85,25 @@ class MainWindow:
     def Learn_Get_Number_File(self):
         files = [f for f in glob.glob(self.Path_Image_Screen_Learn + '/' + "**/*.jpg", recursive=True)]
         self.Number_Image_Learn = len(files)
+
+    def Learn_convert_PDF_TO_IMAGE(self, name):
+        outputDir = self.Path_Image_Screen_Learn + '/'
+        if not os.path.exists(outputDir):
+            os.makedirs(outputDir)
+        pages = convert_from_path(name, 500, size=(self.Learn_Size_X_Image_PDF, self.Learn_Size_Y_Image_PDF))
+        counter = 1
+        for page in pages:
+            myfile = outputDir + self.Name_image_Screen_Learn + str(counter) + '.jpg'
+            counter = counter + 1
+            page.save(myfile, "JPEG")
 #-----------------------------------------(END)-----------------------------------------------------------------------------#
 
+#-----------------------------------------(Giao Diện kiểm tra)-----------------------------------------------------------------------------#
+    def screen_Test_Show_Display_Exam(self):
+        self.uic.stackedWidget_2.setCurrentWidget(self.uic.screen_Test_Display_Exam)
+#-----------------------------------------(END)-----------------------------------------------------------------------------#
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_win =  MainWindow()
     main_win.show()
-
     sys.exit(app.exec())
